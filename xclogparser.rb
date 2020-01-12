88ba90b53ddd2a3d904f4ac11ce3e927b2270ab5
Mario Zigliotto <mariozig@gmail.com>
2020-01-11 19:54:46 -0800

    Move to 0.2.0 version & clean up the formula

--- xclogparser.rb
+++ xclogparser.rb
@@ -1,13 +1,12 @@
 class Xclogparser < Formula
-  desc "Tool to parse the SLF serialization format used by Xcode and xcodebuild to store its Build and Test logs (xcactivitylog)."
-  homepage "http://github.com/spotify/xclogparser"
+  desc 'Tool to parse the SLF serialization format used by Xcode and xcodebuild to store its Build and Test logs (xcactivitylog).'
+  homepage 'https://github.com/spotify/XCLogParser'
+  url 'https://github.com/spotify/XCLogParser.git', tag: '0.2.0'
 
-  url "https://github.com/spotify/xclogparser.git", :revision => "7760731c3342ad6b19d6eb5f4ecf0c1fedbe292b", :tag => "0.1.2"
-  
-  depends_on :xcode => ["10.0", :build]
+  depends_on xcode: ['11.0', :build]
 
   def install
-    system "rake", "install[#{prefix}]"
+    system 'rake', "install[#{prefix}]"
   end
 
   test do
